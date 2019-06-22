47cd8d71ee37e46ba6ebf5bf1f76f00dc68534fe
Luke Karrys <lukekarrys@gmail.com>
2019-06-21 17:10:38 -0700

    test

--- test.js
+++ test.js
@@ -5,24 +5,30 @@ const path = require('path')
 const gpxRouteInspector = require('./index')
 const util = require('./util')
 
-const readdirWithPath = async dir => {
-  const files = await promisify(fs.readdir)(dir)
-  return files.map(f => path.join(dir, f))
+const testExample = async (name, start) => {
+  const dir = path.resolve(__dirname, 'examples', name, 'routes')
+  const filePaths = (await promisify(fs.readdir)(dir)).map(f =>
+    path.join(dir, f)
+  )
+  const { coordinates } = await gpxRouteInspector({
+    filePaths,
+    start,
+    type: 'geojson'
+  })
+  return {
+    coordinates,
+    distance: util.trackDistance(
+      coordinates.map(c => ({ lat: c[1], lon: c[0] }))
+    ),
+    aeg: util.trackAeg(coordinates.map(c => ({ ele: c[2] })))
+  }
 }
 
 test('santan', async t => {
-  const { coordinates } = await gpxRouteInspector({
-    filePaths: await readdirWithPath(
-      path.resolve(__dirname, 'examples', 'santan', 'routes')
-    ),
-    start: 'GOLDMINE|END',
-    type: 'geojson'
-  })
-
-  const distance = util.trackDistance(
-    coordinates.map(c => ({ lat: c[1], lon: c[0] }))
+  const { coordinates, aeg, distance } = await testExample(
+    'santan',
+    'GOLDMINE|END'
   )
-  const aeg = util.trackAeg(coordinates.map(c => ({ ele: c[2] })))
 
   t.deepEqual(coordinates.length, 1190)
   t.deepEqual(distance.toFixed(3), '39208.974')
@@ -30,18 +36,10 @@ test('santan', async t => {
 })
 
 test('usery', async t => {
-  const { coordinates } = await gpxRouteInspector({
-    filePaths: await readdirWithPath(
-      path.resolve(__dirname, 'examples', 'usery', 'routes')
-    ),
-    start: 'PASS-MOUNTAIN',
-    type: 'geojson'
-  })
-
-  const distance = util.trackDistance(
-    coordinates.map(c => ({ lat: c[1], lon: c[0] }))
+  const { coordinates, aeg, distance } = await testExample(
+    'usery',
+    'PASS-MOUNTAIN'
   )
-  const aeg = util.trackAeg(coordinates.map(c => ({ ele: c[2] })))
 
   t.deepEqual(coordinates.length, 1589)
   t.deepEqual(distance.toFixed(3), '66895.779')
